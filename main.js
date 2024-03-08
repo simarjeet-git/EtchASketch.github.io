01f761d79ab065736950264619083b967fb3a043
Simarjeet-git <simarpersonal0@gmail.com>
2024-03-08 12:04:57 +1100

    final product

--- main.js
+++ main.js
@@ -1,28 +1,55 @@
  
- var a;
+
+let  boxcreate = [];
  
  
-function getnumber(){ 
-    a = prompt('please put nmber of boxes you want ..put inbetween 250 to 260')
+let numbervalue =  document.getElementById('nsubmit').addEventListener('click',nv)
+ 
+
+function nv(){
+    let boxnumber =  document.getElementById('boxnumber').value
+ 
+    /*let gamecontainer = document.getElementById('gamecontainer')
+    gamecontainer.style.width =(parseInt("40px".replace(/px/,""))*boxnumber)+"px";
+    gamecontainer.style.height = (parseInt("40px".replace(/px/,""))*boxnumber)+"px";*/
+    for(let i = 0; i<=boxnumber; i++){
+   let  boxdiv =  document.createElement("div")
+    boxdiv.id = "item";
+   gamecontainer.appendChild(boxdiv)
+    boxcreate.push(boxdiv)
+    }
+ 
 }
-getnumber()
-const containertarget = document.querySelector('#container');
-
-
- for(i=0;i<= a;i++){
-    let boxes = document.createElement('div');
-    boxes.id = 'item'
-    containertarget.appendChild(boxes)
-
- }
-
-
-  
- const colorchange = document.querySelectorAll('#item');
- colorchange.forEach(colorchange => {
-     colorchange.addEventListener('mouseover', () => {
-         colorchange.style.backgroundColor = 'yellow';
-     });
- });
-   
  
+let colorchoose = document.getElementById("colorselect").addEventListener('click',()=>{
+    let getcolor = document.getElementById('Color').value
+     
+     boxcreate.forEach(boxcreate=>{
+        boxcreate.addEventListener('mouseover',()=>{
+            boxcreate.style.backgroundColor = `${getcolor}`;
+        })
+     })
+})
+  
+let randomcolor = document.getElementById('rcolor').addEventListener('click',()=>{
+ 
+        let colors = '0123456789ABCDEF';
+        let  color = '#';
+        for (let i = 0; i < 6; i++) {
+          color += colors[Math.floor(Math.random() * 16)];
+           
+        }
+         
+         
+        boxcreate.forEach(boxcreate=>{
+            boxcreate.addEventListener('mouseover',()=>{
+                boxcreate.style.backgroundColor = `${color}`;
+            })
+        })
+    });
+    let gamerestart =  document.getElementById("reset") 
+    gamerestart.addEventListener('click',()=>{
+        location.reload()
+    })
+    
+       
